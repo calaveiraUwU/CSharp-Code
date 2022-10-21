19940a818f6c1926e5aff08819b59679fe1b3899
RaulCano <91997608+calaveiraUwU@users.noreply.github.com>
2022-10-21 22:54:12 +0200

    Update DestroyBehaviour.cs

--- DestroyBehaviour.cs
+++ DestroyBehaviour.cs
@@ -6,11 +6,14 @@ public class DestroyBehaviour : MonoBehaviour
 {
     [SerializeField]
     private GameObject FXSystem;
+    //^Atributo público des de el editor para arrastrarle el prefab de las particulas.
     public void DestroyObject()
     {
+        //Comprobamos si las particulas no están vacias.
         if(FXSystem)
+            //Comprueba si el componente de el sistema de particulas existe.
             if (FXSystem.TryGetComponent<ParticleSystem>(out ParticleSystem p))
-                  Instantiate(p, transform.position, Quaternion.identity);
-        Destroy(gameObject);
+                  Instantiate(p, transform.position, Quaternion.identity);//Aquí instanciamos las particulas en la misma ubicación que el objeto.
+        Destroy(gameObject);//Destruimos el objeto asociado.
     }
 }
