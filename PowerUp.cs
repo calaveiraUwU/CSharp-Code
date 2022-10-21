b7586042f0d9393147111a5a1bbca6ba9cf70521
RaulCano <91997608+calaveiraUwU@users.noreply.github.com>
2022-10-21 23:02:19 +0200

    Update PowerUp.cs

--- PowerUp.cs
+++ PowerUp.cs
@@ -5,23 +5,23 @@ using UnityEngine;
 public abstract class PowerUp : ScriptableObject
 {
     [SerializeField]
-    private HealthBehaviour hb;
-    private GameObject _collission;
+    private HealthBehaviour hb; //Declaramos un objeto de tipo gestor de vida que nosotros hemos creado y lo podamos gestionar des de el editor de Unity.
+    private GameObject _collission; //Un objeto vacio para asignarle la colisión.
 
     public abstract void Apply();
 
     private void OnTriggerEnter(Collider other)
     {
-        _collission = other.gameObject;
+        _collission = other.gameObject; //Cuando un objeto entra en la colisión recolectaremos cual es la colisión y le restaremos vida.
         hb.Hurt();
     }
 
-    public GameObject GetCollision()
+    public GameObject GetCollision() //Función para retornar la colisión.
     {
         return _collission;
     }
 
-    public void SetCollision(GameObject other)
+    public void SetCollision(GameObject other) //Poder establlecer la colisión des de otro script si es neccesario.
     {
         _collission = other;
     }
